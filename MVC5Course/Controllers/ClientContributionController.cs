6302bb02f6e90bd4d27c4d9fafde947ce285146c
chaolong0129 <chaolong.chen@gmail.com>
2016-10-16 15:55:19 +0800

    新增 VW_ClientContribution 檢視表，並更新資料庫模型，新增 ClientContribution Controller 與  View

---
+++ MVC5Course/Controllers/ClientContributionController.cs
@@ -0,0 +1,21 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ClientContributionController : Controller
+    {
+        private FabricsEntities db = new FabricsEntities();
+        // GET: ClientContribution
+        public ActionResult Index()
+        {
+            var vw_cc = db.vw_ClientContribution.Take(10);
+            
+            return View(vw_cc);
+        }
+    }
+}
